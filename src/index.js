84e95258668319ed70072646fa2fa6c1cf45d2ec
ahmed-farid <ahmed.farid0002@gmail.com>
2022-06-08 21:28:48 +0200

    CHECKOUT-PAGE

--- src/index.js
+++ src/index.js
@@ -18,4 +18,48 @@ $(function () {
         $(this).parents('.product-option').siblings().removeClass('active');
         $(this).parents('.product-option').addClass('active');
     });
+
+    // عند تغيير العنصر المطلوب
+    $('[data-product-quantity]').change(function(){
+        // اجلب الكمية المحدده
+        var newQuantity= $(this).val();
+        // ابحث السطر اللذي يحتوي معلومات المنتج
+        var parent = $(this).parents('[data-product-info]');
+        // اجلب سعر القطعة الواحده من معلومات المنتج
+        var pricePerUnit = parent.attr('data-product-price');
+        // السعر الاجمالي للمنتج هو سعر القطعة مضروب بعددها
+        var totalPriceForProduct = newQuantity * pricePerUnit;
+        // السعر الجديد ضمن خلية السعر الاجمالي للمنتج في هذا السطر 
+        parent.find('.total-price-for-product').text(totalPriceForProduct + '$');
+
+        // حدث السعر الاجمالي لكل المنتجات
+        calculateTotalPrice();
+    });
+
+    // زر الحذف
+    $('[data-remove-from-cart]').click(function(){
+        $(this).parents('[data-product-info]').remove();
+        // اعد تحيدث السعر الاجمالي لكل المنتجات 
+        calculateTotalPrice();
+    })
+    // لحساب السعر الاجمالي
+    function calculateTotalPrice() {
+        // انشئ متغير جديد لحفظ السعر الاجمالي
+        var totalPriceForAllProducts = 0;
+        // لكل سطر معلومات المنتج في الصفحه
+        $('[data-product-info]').each(function() {
+            // اجلب سعر القطعة الواحده من معلومات المنتج
+             var pricePerUnit = $(this).attr('data-product-price');
+            // اجلب كمية المنتج من حقل اختيار الكمية
+            var quantity = $(this).find('[data-product-quantity]').val();
+
+            var totalPriceForProduct = pricePerUnit * quantity;
+
+            // اضف السعر الاجالي لهذا لكل المنتجات واحفظ القيمه في المتغير
+            totalPriceForAllProducts = totalPriceForAllProducts + totalPriceForProduct;
+        });
+
+        // حدث السعر الاجالي لكل المنتجات في الصفحه
+        $('#total-price-for-all-products').text(totalPriceForAllProducts + '');
+    }
 });
